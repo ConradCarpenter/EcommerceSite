bb1e0387dbed72748dc947479cd0f468f73abca4
Peter Mooney <peter.mooney90@gmail.com>
2017-10-11 20:45:12 -0400

    Scaffold out POM for AutoTraderDetail view
    - Get photo and title from detail view

--- EcommerceSite/Scraper/Pages/AutoTraderDetail.cs
+++ EcommerceSite/Scraper/Pages/AutoTraderDetail.cs
@@ -1,10 +1,31 @@
 ﻿using System;
-namespace EcommerceSite.Scraper
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace EcommerceSite.Scraper.Pages
 {
     public class AutoTraderDetail
     {
-        public AutoTraderDetail()
+        IWebDriver driver;
+        const string baseURL = "https://www.autotrader.com/cars-for-sale/vehicledetails.xhtml?listingId=";
+
+        public AutoTraderDetail(IWebDriver driver)
         {
+            this.driver = driver;
+        }
+
+        public void NavigateListing(int listingId) {
+            driver.Url = baseURL + listingId;
+        }
+
+        public String GetMainPhoto() {
+            IWebElement picture = driver.FindElement(By.XPath("//div[contains(@class, 'media-viewer')]//img"));
+            return picture.GetAttribute("src");
+        }
+
+        public String GetTitle() {
+            IWebElement title = driver.FindElement(By.XPath("//div[@data-qaid='cntnr-vehicle-title-header']"));
+            return title.Text;
         }
     }
 }
